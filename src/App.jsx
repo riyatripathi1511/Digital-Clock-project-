b18b90c72e1148f6718a3ee2cf88672bb8630c60
Riya Tripathi <riyatripathi1511@gmail.com>
2024-09-16 00:36:14 +0530

    dig-clock

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import DigitalClock from './DigitalClock'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <DigitalClock>
+        
+      </DigitalClock>
+    </>
+  )
+}
+
+export default App
